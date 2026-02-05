5fd6e2bb26d94284581023361e4890f220cbb39e
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-05 14:43:31 +0530

    WMDT-250 fix: all previous junk cleared and fixed, also tested

--- src/warehouse/warehouse.controller.ts
+++ src/warehouse/warehouse.controller.ts
@@ -1,45 +1,36 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { WarehouseService } from './warehouse.service';
 import User from './types/userType';
+import type { Request } from 'express';
+import { AuthGuard } from 'src/common/guard/auth.guard';
 
-@Controller()
+export interface RequestWithUser extends Request {
+  user: User;
+}
+
+@UseGuards(AuthGuard)
+@Controller('warehouse/')
 export class WarehouseController {
   constructor(private readonly service: WarehouseService) {}
 
   @Get('get-warehouses')
-  getWarehouses(@Req() req: Request & User) {
-    const user = {
-      _id: 'TEMP_USER_ID',
-      role: 'ADMIN', // or MANAGER
-      name: 'Temp User',
-      email: 'temp@test.com',
-    };
-
-    return this.service.getWarehouses(user); // req.user
+  getWarehouses(@Req() req: RequestWithUser) {
+    return this.service.getWarehouses(req.user);
   }
 
   @Get('get-warehouses/:warehouseId')
   getWarehouseById(
     @Param('warehouseId') warehouseId: string,
-    // @Req() req: Request,
+    @Req() req: RequestWithUser,
   ) {
-    const user = {
-      _id: 'TEMP_USER_ID',
-      role: 'ADMIN',
-    };
-
-    return this.service.getWarehouseById(warehouseId, user); // req.user
+    return this.service.getWarehouseById(warehouseId, req.user);
   }
 
   @Get('get-warehouse-capacity/:warehouseId')
   getWarehouseCapacity(
     @Param('warehouseId') warehouseId: string,
-    // @Req() req: Request,
+    @Req() req: RequestWithUser,
   ) {
-    const user = {
-      _id: 'TEMP_USER_ID',
-      role: 'ADMIN',
-    };
-    return this.service.getWarehouseCapacity(warehouseId, user); // req.user
+    return this.service.getWarehouseCapacity(warehouseId, req.user);
   }
 }
