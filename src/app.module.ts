c4c575fedc17cb8892822bfd534e35a435dcb62c
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-05 10:25:21 +0530

    WMDT-250 feat: warehouse module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { DbModule } from './config/db.module';
+import { WarehouseModule } from './warehouse/warehouse.module';
 
 @Module({
-  imports: [ConfigModule, DbModule],
+  imports: [ConfigModule, DbModule, WarehouseModule],
   controllers: [AppController],
   providers: [AppService],
 })
