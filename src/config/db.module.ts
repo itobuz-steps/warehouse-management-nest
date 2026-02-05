a630b282c77cd9734cd5b47dbce5286c847128be
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-05 10:26:26 +0530

    WMDT-250 fix: no need for eslint error suppressing

--- src/config/db.module.ts
+++ src/config/db.module.ts
@@ -23,8 +23,7 @@ export class DbModule implements OnModuleInit {
       this.logger.log('MongoDB connected successfully');
     } else {
       this.logger.error(
-        // eslint-disable-next-line prettier/prettier
-        `MongoDB connection state: ${ConnectionStates[this.connection.readyState]}`
+        `MongoDB connection state: ${ConnectionStates[this.connection.readyState]}`,
       );
     }
   }
