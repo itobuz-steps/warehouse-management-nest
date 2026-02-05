136c810a852bc3f2fc48df8d63e3e2e2ce203293
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-05 10:27:40 +0530

    WMDT-250 feat: module

---
+++ src/warehouse/warehouse.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { WarehouseController } from './warehouse.controller';
+import { WarehouseService } from './warehouse.service';
+import { Warehouse, WarehouseSchema } from './schemas/warehouse.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Warehouse.name, schema: WarehouseSchema },
+    ]),
+  ],
+  controllers: [WarehouseController],
+  providers: [WarehouseService],
+})
+export class WarehouseModule {}
