5ad0e9c59f236b3ecef08628ed8c883d0c162113
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-06 13:15:56 +0530

    WMDT-257 feat: admin controller and service

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,39 +1,18 @@
-import {
-  Controller,
-  Post,
-  Patch,
-  Delete,
-  Get,
-  Param,
-  Body,
-} from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
-import { AddWarehouseDto } from './dto/add-warehouse.dto';
-import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
 import { USER_TYPES } from 'src/auth/userType';
 import { Roles } from 'src/common/guard/roles.decorator';
+import { AuthGuard } from 'src/common/guard/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('admin')
-@Roles(USER_TYPES.ADMIN)
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Post('warehouses')
-  addWarehouse(@Body() dto: AddWarehouseDto) {
-    return this.adminService.addWarehouse(dto);
-  }
-
-  @Patch('warehouses/:id')
-  updateWarehouse(@Param('id') id: string, @Body() dto: UpdateWarehouseDto) {
-    return this.adminService.updateWarehouse(id, dto);
-  }
-
-  @Delete('warehouses/:id')
-  removeWarehouse(@Param('id') id: string) {
-    return this.adminService.removeWarehouse(id);
-  }
-
   @Get('get-managers')
+  @Roles(USER_TYPES.ADMIN)
   getManagers() {
     return this.adminService.getManagers();
   }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,62 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import mongoose from 'mongoose';
-import { AddWarehouseDto } from './dto/add-warehouse.dto';
-import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
 import { USER_TYPES } from 'src/auth/userType';
-import { Warehouse } from 'src/warehouse/schemas/warehouse.schema';
 import { User } from 'src/auth/entities/auth.entity';
 
 @Injectable()
 export class AdminService {
   constructor(
-    @InjectModel(Warehouse.name)
-    private readonly warehouseModel: Model<Warehouse>,
-
     @InjectModel(User.name)
     private readonly userModel: Model<User>,
   ) {}
-
-  async addWarehouse(dto: AddWarehouseDto) {
-    const managerIds = dto.managers?.map(
-      (id) => new mongoose.Types.ObjectId(id),
-    );
-
-    return this.warehouseModel.create({
-      ...dto,
-      managerIds,
-    });
-  }
-
-  async updateWarehouse(id: string, dto: UpdateWarehouseDto) {
-    const managerIds = dto.managers?.map(
-      (id) => new mongoose.Types.ObjectId(id),
-    );
-
-    const warehouse = await this.warehouseModel.findByIdAndUpdate(
-      id,
-      { ...dto, managerIds },
-      { new: true },
-    );
-
-    if (!warehouse) {
-      throw new NotFoundException('Warehouse not found');
-    }
-
-    return warehouse;
-  }
-
-  async removeWarehouse(id: string) {
-    const warehouse = await this.warehouseModel.findOneAndUpdate(
-      { _id: id, active: true },
-      { active: false },
-    );
-
-    if (!warehouse) {
-      throw new NotFoundException('Warehouse not found');
-    }
-  }
 
   async getManagers() {
     return this.userModel.find({
