e16fac134499ef0072f0fc0f4227ba5faf64c382
Sanket Ganguly <sanket.ganguly@itobuz.com>
2026-02-06 13:58:51 +0530

    WMDT-255 fix: multer file upload constant parameters with path module

--- src/common/constants/file.constant.ts
+++ src/common/constants/file.constant.ts
@@ -1,12 +1,12 @@
-export const FOLDER_PATH = {
-  uploads: 'uploads',
-  product: 'product',
-  user: 'user',
-};
+export enum FOLDER_PATH {
+  uploads = 'uploads',
+  product = 'product',
+  user = 'user',
+}
 
-export const FILE_FIELD = {
-  productImage: 'productImage',
-  profileImage: 'profileImage',
-};
+export enum FILE_FIELD {
+  productImage = 'productImage',
+  profileImage = 'profileImage',
+}
 
 export const FILE_COUNT = 8;
