b88c10237460ed5ba35eaa8ae1db6e01e60f19a2
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-06 13:19:19 +0530

    WMDT-257 feat: add warehouse, update warehouse, delete warehouse added in warehouse controller

--- src/warehouse/warehouse.controller.ts
+++ src/warehouse/warehouse.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { WarehouseService } from './warehouse.service';
 import type { RequestWithUser } from './types/userType';
 import { AuthGuard } from 'src/common/guard/auth.guard';
+import { CreateWarehouseDto } from './dto/create-warehouse.dto';
+import { USER_TYPES } from 'src/auth/userType';
+import { Roles } from 'src/common/guard/roles.decorator';
+import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 @Controller('warehouse/')
 export class WarehouseController {
   constructor(private readonly service: WarehouseService) {}
@@ -27,5 +43,23 @@ export class WarehouseController {
     @Req() req: RequestWithUser,
   ) {
     return this.service.getWarehouseCapacity(warehouseId, req.user);
+  }
+
+  @Post()
+  @Roles(USER_TYPES.ADMIN)
+  addWarehouse(@Body() dto: CreateWarehouseDto) {
+    return this.service.addWarehouse(dto);
+  }
+
+  @Patch('/:id')
+  @Roles(USER_TYPES.ADMIN)
+  updateWarehouse(@Param('id') id: string, @Body() dto: UpdateWarehouseDto) {
+    return this.service.updateWarehouse(id, dto);
+  }
+
+  @Delete('/:id')
+  @Roles(USER_TYPES.ADMIN)
+  deleteWarehouse(@Param('id') id: string) {
+    return this.service.deleteWarehouse(id);
   }
 }
