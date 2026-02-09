45d50be32eb6fa280e6fd7f0b316e3986cb13c90
Shaswata Biswas <shaswata.biswas@itobuz.com>
2026-02-09 15:53:10 +0530

    WMDT-259 added role guard to required routes

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -8,9 +8,14 @@ import {
   Min,
   Max,
   IsBoolean,
+  IsMongoId,
 } from 'class-validator';
 
 export class updateProductDto {
+  @IsNotEmpty()
+  @IsMongoId({ message: 'The provided ID is not a valid MongoDB ObjectId' })
+  id: string;
+
   @IsString()
   @IsNotEmpty()
   name: string;
