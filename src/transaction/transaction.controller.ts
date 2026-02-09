e0bb7a2820bfc66563af6e4c813cc78b25facbc1
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-09 17:16:16 +0530

    WMDT-261 feat: stock in, stock out, transfer, adjustment controller

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,19 +1,22 @@
 import {
   Controller,
   Get,
-  // Post,
-  // Body,
+  Post,
+  Body,
   Query,
   Param,
   UseGuards,
   Req,
 } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
-// import { StockInDto } from './dto/stock-in.dto';
+import { StockInDto } from './dto/stock-in.dto';
 import { AuthGuard } from 'src/common/guard/auth.guard';
 import { GetTransactionsQueryDto } from './dto/query/get-transactions.query.dto';
 import { WarehouseTransactionsQueryDto } from './dto/query/warehouse-transactions.query.dto';
 import type { RequestWithUserDocument } from './types/types';
+import { StockOutDto } from './dto/stock-out.dto';
+import { TransferDto } from './dto/transfer.dto';
+import { AdjustmentDto } from './dto/adjustment.dto';
 
 @Controller('transactions')
 @UseGuards(AuthGuard)
@@ -36,10 +39,34 @@ export class TransactionController {
     return this.transactionService.getWarehouseTransactions(warehouseId, query);
   }
 
-  // @Post('stock-in')
-  // createStockIn(@Body() dto: StockInDto, @Req() req: RequestWithUserDocument) {
-  //   return this.transactionService.createStockIn(dto, req.user.id);
-  // }
+  @Post('stock-in')
+  createStockIn(@Req() req: RequestWithUserDocument, @Body() dto: StockInDto) {
+    return this.transactionService.createStockIn(dto, req.user.id);
+  }
+
+  @Post('stock-out')
+  createStockOut(
+    @Req() req: RequestWithUserDocument,
+    @Body() dto: StockOutDto,
+  ) {
+    return this.transactionService.createStockOut(dto, req.user.id);
+  }
+
+  @Post('transfer')
+  createTransfer(
+    @Req() req: RequestWithUserDocument,
+    @Body() dto: TransferDto,
+  ) {
+    return this.transactionService.createTransfer(dto, req.user.id);
+  }
+
+  @Post('adjustment')
+  createAdjustment(
+    @Req() req: RequestWithUserDocument,
+    @Body() dto: AdjustmentDto,
+  ) {
+    return this.transactionService.createAdjustment(dto, req.user.id);
+  }
 
   @Get('generate-invoice/:id')
   generateInvoice(@Param('id') id: string) {
