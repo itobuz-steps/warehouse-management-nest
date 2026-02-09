daa4834a80268aff0ad06a32f11d3f3d800c5396
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-09 17:19:47 +0530

    WMDT-261 feat: stock out dto

--- src/transaction/dto/stock-out.dto.ts
+++ src/transaction/dto/stock-out.dto.ts
@@ -1,4 +1,41 @@
-// import { PartialType } from '@nestjs/swagger';
-// import { CreateTransactionDto } from './stock-in.dto';
+import {
+  IsArray,
+  IsEmail,
+  IsMongoId,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ProductItemDto } from './product-item.dto';
 
-// export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
+export class StockOutDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ProductItemDto)
+  products: ProductItemDto[];
+
+  @IsOptional()
+  @IsString()
+  customerName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  customerEmail?: string;
+
+  @IsOptional()
+  @IsNumber()
+  customerPhone?: number;
+
+  @IsOptional()
+  @IsString()
+  customerAddress?: string;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+
+  @IsMongoId()
+  sourceWarehouse: string;
+}
