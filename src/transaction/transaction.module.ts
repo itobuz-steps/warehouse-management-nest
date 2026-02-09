fc0ccd8c655bf5f9f46af1247393c3f639e18cb2
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-09 17:17:22 +0530

    WMDT-261 fix: transaction module changes

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,21 +3,28 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
 import { Transaction, TransactionSchema } from './schemas/transaction.schema';
-// import { Quantity, QuantitySchema } from '../quantity/schemas/quantity.schema';
 import {
   Warehouse,
   WarehouseSchema,
 } from '../warehouse/schemas/warehouse.schema';
 import { User, UserSchema } from 'src/auth/entities/auth.entity';
 import { PdfService } from './services/pdf.service';
+import {
+  Quantity,
+  QuantitySchema,
+} from 'src/quantity/entities/quantity.entity';
+import { ConfigModule } from '@nestjs/config';
+import { Product, ProductSchema } from 'src/products/entities/product.entity';
 // import Notification from '../utils/Notification';
 
 @Module({
   imports: [
+    ConfigModule,
     MongooseModule.forFeature([
       { name: Transaction.name, schema: TransactionSchema },
-      // { name: Quantity.name, schema: QuantitySchema },
+      { name: Quantity.name, schema: QuantitySchema },
       { name: Warehouse.name, schema: WarehouseSchema },
+      { name: Product.name, schema: ProductSchema },
       { name: User.name, schema: UserSchema },
     ]),
   ],
