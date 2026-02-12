27883d1f5a1dd777788efdd2d42f6469e8c6779a
Shaswata Biswas <shaswata.biswas@itobuz.com>
2026-02-12 17:14:11 +0530

    fix: WMDT-270 removed blank import

--- src/dashboard/types/dashboard.data.type.ts
+++ src/dashboard/types/dashboard.data.type.ts
@@ -1,5 +1,4 @@
 import { Types } from 'mongoose';
-import {} from 'src/products/dto/create-product.dto';
 import { Product } from 'src/products/entities/product.entity';
 
 export type TopProductItem = {
