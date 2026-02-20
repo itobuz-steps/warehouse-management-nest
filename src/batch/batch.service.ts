f6509d3e880f340e078ab9a01d517fffdbf7d83b
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-20 10:46:28 +0530

    feat: WMDT-274 batch services added

--- src/batch/batch.service.ts
+++ src/batch/batch.service.ts
@@ -1,28 +1,106 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Batch, BatchDocument } from './schemas/batch.schema';
 import { CreateBatchDto } from './dto/create-batch.dto';
-import { UpdateBatchDto } from './dto/update-batch.dto';
+import { Variant, VariantDocument } from '../variant/schemas/variant.schema';
+import { TRANSACTION_TYPES } from 'src/transaction/constants/transactionConstants';
+
+type BatchProductItem = {
+  variant: Types.ObjectId;
+  quantity: number;
+};
 
 @Injectable()
 export class BatchService {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  create(createBatchDto: CreateBatchDto) {
-    return 'This action adds a new batch';
+  constructor(
+    @InjectModel(Batch.name)
+    private readonly batchModel: Model<BatchDocument>,
+
+    @InjectModel(Variant.name)
+    private readonly variantModel: Model<VariantDocument>,
+  ) {}
+
+  private validateWarehouses(
+    type: TRANSACTION_TYPES,
+    source?: string,
+    destination?: string,
+  ) {
+    if (type === TRANSACTION_TYPES.TRANSFER) {
+      if (!source || !destination) {
+        throw new BadRequestException(
+          'Transfer requires both source and destination warehouses',
+        );
+      }
+      if (source === destination) {
+        throw new BadRequestException(
+          'Source and destination cannot be the same',
+        );
+      }
+    }
+
+    if (type === TRANSACTION_TYPES.IN && !destination) {
+      throw new BadRequestException('Stock In requires destination warehouse');
+    }
+
+    if (type === TRANSACTION_TYPES.OUT && !source) {
+      throw new BadRequestException('Stock Out requires source warehouse');
+    }
   }
 
-  findAll() {
-    return `This action returns all batch`;
+  async create(dto: CreateBatchDto) {
+    this.validateWarehouses(
+      dto.transactionType,
+      dto.sourceWarehouse,
+      dto.destinationWarehouse,
+    );
+
+    const preparedItems: BatchProductItem[] = [];
+
+    for (const item of dto.items) {
+      preparedItems.push({
+        variant: new Types.ObjectId(item.variant),
+        quantity: item.quantity,
+      });
+    }
+
+    const batch = new this.batchModel({
+      transactionType: dto.transactionType,
+      sourceWarehouse: new Types.ObjectId(dto.sourceWarehouse),
+      destinationWarehouse: new Types.ObjectId(dto.destinationWarehouse),
+      items: preparedItems,
+    });
+
+    return {
+      success: true,
+      message: 'Batch created successfully',
+      data: await batch.save(),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} batch`;
+  async findAll() {
+    return {
+      success: true,
+      message: 'Batches retrieved successfully',
+      data: await this.batchModel
+        .find()
+        .populate('sourceWarehouse')
+        .populate('destinationWarehouse')
+        .populate('items.variant')
+        .sort({ createdAt: -1 }),
+      // .lean(); // will implement later if needed for performance optimization
+    };
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  update(id: number, updateBatchDto: UpdateBatchDto) {
-    return `This action updates a #${id} batch`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} batch`;
+  async findOne(id: string) {
+    return {
+      success: true,
+      message: 'Batch retrieved successfully',
+      data: await this.batchModel
+        .findById(id)
+        .populate('sourceWarehouse')
+        .populate('destinationWarehouse')
+        .populate('items.variant'),
+    };
   }
 }
