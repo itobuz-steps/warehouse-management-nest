1e3ca35d87290126cb5621326cde4d02c83bb036
Sohan Chatterjee <sohan.chatterjee@itobuz.com>
2026-02-20 10:45:19 +0530

    fix: WMDT-274 changes in modules

--- src/variant/variant.module.ts
+++ src/variant/variant.module.ts
@@ -4,12 +4,14 @@ import { VariantController } from './variant.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Variant, VariantSchema } from './schemas/variant.schema';
 import { Product, ProductSchema } from 'src/products/entities/product.entity';
+import { User, UserSchema } from 'src/auth/entities/auth.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Variant.name, schema: VariantSchema },
       { name: Product.name, schema: ProductSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [VariantController],
