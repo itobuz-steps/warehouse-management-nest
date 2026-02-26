6c83728fd55df37e1e78aaf1afe76af4e421347e
Shaswata Biswas <shaswata.biswas@itobuz.com>
2026-02-26 12:38:21 +0530

    fix: WMDT-313 added status update for customer

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -36,7 +36,7 @@ export class CustomerService {
   }
 
   async findAll() {
-    return await this.customerModel.find({ isActive: true }).exec();
+    return await this.customerModel.find().exec();
   }
 
   async findOne(id: string) {
